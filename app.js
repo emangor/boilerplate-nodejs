e7bba7d3f918086b203f94de380fd2c0311c29ad
emangor <emangor@gmail.com>
2017-07-30 20:19:42 -0400

    add cpu clustering

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const config = require( './config' );
 const express = require('express');
-module.exports.express = express;
-const app = express();
-module.exports.app = app;
-const routes = require('./routes');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const logger = require('./utils/logger');
-app.listen(config.port);
-logger.info(`${config.serviceName} is listening on port: ${config.port}`);
+
+if (cluster.isMaster) {
+    // create a worker for each CPU
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('online', (worker) => {
+        logger.info(`worker online, worker id: ${worker.id}`);
+    });
+    //if worker dies, create another one
+    cluster.on('exit', (worker, code, signal) => {
+        logger.error(`worker died, worker id: ${worker.id} | signal: ${signal} | code: ${code}`);
+        cluster.fork();
+    });
+} else {
+    //create express app
+    module.exports.express = express;
+    const app = express();
+    module.exports.app = app;
+    const routes = require('./routes');
+    const logger = require('./utils/logger');
+    app.listen(config.port, function () {
+        logger.info(`worker started: ${cluster.worker.id} | server listening on port: ${config.port}`);
+    });
+}
