df9e79590c58974800d5e694b9a21aab10d63216
emangor <emangor@gmail.com>
2019-02-28 15:36:54 +0100

    remove unneeded routes file and add to app.js - update node version

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const logger = require('./utils/logger');
+const bodyParser = require('body-parser');
+//import controllers
+const healthcheckController = require('./controllers/controller-healthcheck');
+const userController = require('./controllers/controller-user');
 
 if (cluster.isMaster) {
     // create a worker for each CPU
@@ -19,10 +23,17 @@ if (cluster.isMaster) {
     });
 } else {
     //create express app
-    module.exports.express = express;
     const app = express();
-    module.exports.app = app;
-    const routes = require('./routes');
+    const router = express.Router();
+
+    app.use(bodyParser.json());
+    app.use(router);
+    //healthcheck routes
+    router.get('/', healthcheckController.healthcheck);
+    router.get('/healthcheck', healthcheckController.healthcheck);
+    // userController routes
+    router.get('/user/:username', userController.getUser);
+
     app.listen(config.port, function () {
         logger.info(`worker started: ${cluster.worker.id} | server listening on port: ${config.port}`);
     });
